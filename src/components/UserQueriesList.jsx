b4b3ca1047648d8d56798ea04e222bd5ed2a7d68
edgar1006-cyber <edgarpeinado33@gmail.com>
2025-09-26 15:56:00 -0400

    Trabajo2_commit

--- src/components/UserQueriesList.jsx
+++ src/components/UserQueriesList.jsx
@@ -10,14 +10,21 @@ const UserQueriesList = ({ usuarios, Loading }) => {
                 {usuarios.length === 0 ? (
                     <p>No hay usuarios disponibles.</p>
                 ) : (
-                    usuarios.map((usuario) => (
-                        <div key={usuario.id} className="user-item">
-                            <p><strong>ID:</strong> {usuario.id}</p>
-                            <p><strong>Nombre:</strong> {usuario.nombre}</p>
-                            <p><strong>Edad:</strong> {usuario.edad}</p>
-                            <p><strong>Email:</strong> {usuario.email}</p>
-                        </div>
-                    ))
+                    usuarios.map((usuario) => {
+                        
+                        const hiddenPassword = "*".repeat(usuario.password?.length || 0);
+
+                        return (
+                            <div key={usuario.id} className="user-item">
+                                <p><strong>ID:</strong> {usuario.id}</p>
+                                <p><strong>Nombre:</strong> {usuario.nombre}</p>
+                                <p><strong>Edad:</strong> {usuario.edad}</p>
+                                <p><strong>Email:</strong> {usuario.email}</p>
+                                <p><strong>Username:</strong> {usuario.username}</p>
+                                <p><strong>Password:</strong> {hiddenPassword}</p>
+                            </div>
+                        );
+                    })
                 )}
             </div>
         </div>
